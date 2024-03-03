64431c779bac3c0eef7cd155e5204f2c229040d4
Shay <shay.gol55@gmail.com>
2024-03-03 19:01:43 +0200

    first commit

---
+++ src/components/Transactions.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+
+import { Trash2, Pencil } from "lucide-react";
+
+import { getIcon } from "../lib/utils";
+
+
+export const Transactions = ({ items ,setIsOpen, isOpen}) => {
+  const [rating, setRating] = useState(0);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [expenseToDelete, setExpenseToDelete] = useState(null);
+  const [expenseToEdit, setExpenseToEdit] = useState(null);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState(""); // State variable for search query
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  const [expenses, setExpenses] = useState([]);
+
+
+
+  const closeDeleteModal = () => {
+    setExpenseToDelete(null);
+    setIsDeleteModalOpen(false);
+  };
+
+
+
+  const closeEditModal = () => {
+    setIsEditModalOpen(false);
+    setExpenseToEdit(null);
+  };
+
+  const handleDelete = () => {
+    if (!expenseToDelete) return;
+
+    const updatedExpenses = expenses.filter((expense) => expense.id !== expenseToDelete.id);
+    setExpenses(updatedExpenses);
+
+    localStorage.setItem('expenses', JSON.stringify(updatedExpenses));
+
+    setIsDeleteModalOpen(false);
+  };
+
+  return (
+    <>
+    {items.map((expense) => (
+          <div
+            className="w-full max-w-md p-6 m-3 text-center text-white transition-transform transform bg-gray-800 border border-gray-700 shadow-lg hover:shadow-xl rounded-xl hover:scale-105 hover:bg-opacity-90 hover:text-gray-100"
+            key={expense.id}
+          >
+            <div className="flex justify-end gap-3">
+              <Trash2
+                className="w-5 h-5 text-red-500 cursor-pointer"
+                onClick={() => openDeleteModal(expense)}
+              />
+              <Pencil
+                className="w-5 h-5 cursor-pointer text-sky-400"
+                onClick={() => openEditModal(expense)}
+              />
+            </div>
+            <span className="text-2xl font-semibold break-words sm:text-xl">
+              {expense.title}
+            </span>
+
+            <div className="flex items-center w-2/3 mt-4 sm:w-full">
+              <span className="px-2 text-xl sm:text-lg sm:px-8 sm:mx-auto">
+                ₪{Number(expense.amount).toFixed(2)}
+              </span>
+            </div>
+            <div className="flex justify-between mt-12">
+              <span className="px-2 mr-auto text-xl text-zinc-500 sm:text-lg">
+                {new Date(expense.date).toLocaleDateString()}
+              </span>
+              <span className="text-zinc-500">{getIcon(expense.rating)}</span>
+            </div>
+          </div>
+        ))}
+    </>
+  )
+}
